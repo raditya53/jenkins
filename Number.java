d83add843e2df092879397780559d76f90c669da
Raditya <radityaarya53@gmail.com>
2021-01-19 12:31:38 +0700

    bilangan prima

--- Number.java
+++ Number.java
@@ -1,9 +1,22 @@
 import java.util.Scanner;
 public class Number {
 public static void main(String args[]) {
-System.out.println("Hello World");
-for (int i=0; i<10; i++){
-System.out.println(i);
-}
-}
+int bil, awal, akhir;
+        
+         System.out.println("PROGRAM JAVA MENAMPILKAN DERET BILANGAN PRIMA");
+		 awal = 1;
+		 akhir = 100;
+         System.out.println("----------------------------------------------");
+        for (int i=awal; i<=akhir; i++){
+            bil=0;
+            for (int j=1;j<=i;j++){
+                if (i%j==0){
+                    bil=bil+1;
+                }
+            }
+            if (bil==2){
+                System.out.print(i+" ");
+            }             
+        }    
+   }
 }
